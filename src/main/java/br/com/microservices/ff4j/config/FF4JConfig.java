fce9b15d68d43e24cf4f533506beb5dcb31f97c6
Everson Gouvea <suporte.gouvea@gmail.com>
2019-11-18 14:00:00 -0300

    Conectado ao consul

--- src/main/java/br/com/microservices/ff4j/config/FF4JConfig.java
+++ src/main/java/br/com/microservices/ff4j/config/FF4JConfig.java
@@ -1,8 +1,11 @@
 package br.com.microservices.ff4j.config;
 
 import org.ff4j.FF4j;
+import org.ff4j.consul.ConsulConnection;
+import org.ff4j.consul.store.FeatureStoreConsul;
 import org.ff4j.web.FF4jDispatcherServlet;
 import org.ff4j.web.embedded.ConsoleServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -10,38 +13,51 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.net.HostAndPort;
+import com.orbitz.consul.Consul;
+
 @Configuration
 @ConditionalOnClass({ ConsoleServlet.class, FF4jDispatcherServlet.class })
-public class FF4JConfig extends SpringBootServletInitializer
-{
+public class FF4JConfig extends SpringBootServletInitializer {
 
+	@Value("${spring.cloud.consul.host:localhost}")
+	private String consulHost;
 
-    @Bean
-    public ServletRegistrationBean<FF4jDispatcherServlet> ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet)
-    {
-        ServletRegistrationBean<FF4jDispatcherServlet> bean = new ServletRegistrationBean<FF4jDispatcherServlet>(ff4jDispatcherServlet, "/*");
-        bean.setName("ff4j-console");
-        bean.setLoadOnStartup(1);
-        return bean;
-    }
+	@Value("${spring.cloud.consul.port:8500}")
+	private int consulPort;
 
-    @Bean
-    @ConditionalOnMissingBean
-    public FF4jDispatcherServlet getFF4jDispatcherServlet()
-    {
-        FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
-        ff4jConsoleServlet.setFf4j(getFF4j());
-        return ff4jConsoleServlet;
-    }
+	@Bean
+	public ServletRegistrationBean<FF4jDispatcherServlet> ff4jDispatcherServletRegistrationBean(
+			FF4jDispatcherServlet ff4jDispatcherServlet) {
+		ServletRegistrationBean<FF4jDispatcherServlet> bean = new ServletRegistrationBean<FF4jDispatcherServlet>(
+				ff4jDispatcherServlet, "/*");
+		bean.setName("ff4j-console");
+		bean.setLoadOnStartup(1);
+		return bean;
+	}
 
-    @Bean
-    public FF4j getFF4j()
-    {
-        FF4j ff4j = new FF4j();
+	@Bean
+	@ConditionalOnMissingBean
+	public FF4jDispatcherServlet getFF4jDispatcherServlet() {
+		FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
+		ff4jConsoleServlet.setFf4j(getFF4j());
+		return ff4jConsoleServlet;
+	}
 
-        ff4j.audit(true);
+	@Bean
+	public FF4j getFF4j() {
 
-        return ff4j;
-    }
+		Consul c = Consul.builder().withHostAndPort(HostAndPort.fromParts(consulHost, consulPort)).build();
+
+		ConsulConnection connection = new ConsulConnection(c);
+		FeatureStoreConsul featureStoreConsul = new FeatureStoreConsul(connection);
+
+		FF4j ff4j = new FF4j();
+		ff4j.audit(true);
+		ff4j.setFeatureStore(featureStoreConsul);
+		ff4j.setPropertiesStore(new org.ff4j.consul.store.PropertyStoreConsul(connection));
+
+		return ff4j;
+	}
 
 }
